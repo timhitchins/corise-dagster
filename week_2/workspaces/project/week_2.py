aa1fdfe4dd084b29150c356826a6c65757607b80
Tim Hitchins <tim.x.hitchins@gmail.com>
2023-04-14 04:30:39 +0000

    week 2 tests passing

--- week_2/workspaces/project/week_2.py
+++ week_2/workspaces/project/week_2.py
@@ -15,19 +15,17 @@ from workspaces.config import REDIS, S3, S3_FILE
 from workspaces.resources import mock_s3_resource, redis_resource, s3_resource
 from workspaces.types import Aggregation, Stock
 
-# replace that functionality with our S3 resource and use the S3 client method get_data
-# to read the contents a file from remote storage
-# (in this case our localstack version  of S3 within Docker).
+
 @op(
     config_schema={"s3_key": String},
     out={"stocks": Out(dagster_type=List[Stock])},
-    required_resource_keys={"s3"},
+    required_resource_keys={"s3"},  # use with context
     tags={"kind": "s3"},
     description="Get stock data from s3 bucket.",
 )
 def get_s3_data(context: OpExecutionContext):
     s3_key = context.op_config["s3_key"]
-    # use the s3 resource key to call client method
+    # use the s3 resource key to call s3 client method get_data
     csv_lines = context.resources.s3.get_data(s3_key)
     stocks_obj = [Stock.from_list(line) for line in csv_lines]
     context.log.info("Fetched s3 stocks data")
@@ -40,37 +38,57 @@ def get_s3_data(context: OpExecutionContext):
     description="Filter and return highest stock (mock aggregation).",
 )
 def process_data(context, stocks):
+    # use max with lambda for highest stock
     high_stock = max(stocks, key=lambda x: x.high)
     aggregation = Aggregation(date=high_stock.date, high=high_stock.high)
+    context.log.info("Aggregation complete")
     return aggregation
-
-
-@op()
-def put_redis_data():
-    pass
 
 
 @op(
     ins={"aggregation": In(dagster_type=Aggregation)},
     out=Out(dagster_type=Nothing),
-    tags={"kind": "s3"},
-    description="Upload aggregation to s3.",
+    required_resource_keys={"redis"},
+    description="Writes to redis cache",
 )
-def put_s3_data(context: OpExecutionContext, aggregation):
-    s3_key = context.op_config["s3_key"]
+def put_redis_data(context: OpExecutionContext, aggregation):
+    name = str(aggregation.date)
+    value = str(aggregation.high)
+    # use the redis resource key to call redis client method put_data
+    context.resources.redis.put_data(name, value)
+    context.log.info("Aggregation written to redis cache")
+
+
+@op(
+    ins={"aggregation": In(dagster_type=Aggregation)},
+    out=Out(dagster_type=Nothing),
+    required_resource_keys={"s3"},
+    tags={"kind": "s3"},
+    description="Aggregation written to s3.",
+)
+def put_s3_data(context, aggregation):
+    # use S3_FILE directly
+    s3_key = S3_FILE
     context.resources.s3.put_data(s3_key, aggregation)
+    context.log.info("Aggregation written to S3")
 
 
 @graph
 def machine_learning_graph():
     s3_data = get_s3_data()
     aggregation = process_data(s3_data)
-    # put_redis_data(aggregation)
+    put_redis_data(aggregation)
     put_s3_data(aggregation)
 
 
+# set resource and op confis to pass to jobs
 local = {
-    "ops": {"get_s3_data": {"config": {"s3_key": S3_FILE}}},
+    "resources": {
+        "redis": {"config": REDIS},
+    },
+    "ops": {
+        "get_s3_data": {"config": {"s3_key": S3_FILE}},
+    },
 }
 
 docker = {
@@ -81,12 +99,13 @@ docker = {
     "ops": {"get_s3_data": {"config": {"s3_key": S3_FILE}}},
 }
 
+# Call job on graph
 machine_learning_job_local = machine_learning_graph.to_job(
     name="machine_learning_job_local",
     config=local,
     resource_defs={"s3": mock_s3_resource, "redis": redis_resource},
 )
 
-# machine_learning_job_docker = machine_learning_graph.to_job(
-#     name="machine_learning_job_docker", config=docker, resource_defs={"s3": s3_resource}
-# )
+machine_learning_job_docker = machine_learning_graph.to_job(
+    name="machine_learning_job_docker", config=docker, resource_defs={"s3": s3_resource, "redis": redis_resource}
+)
